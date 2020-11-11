37b8cbc81d78c1611bc4dada068b78749e58cb9c
Shubha Guha <shubha.g@gmail.com>
2020-11-11 15:51:23 +0100

    Separated create_app function

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 
 import dash
 from dash.dependencies import Input, Output, State
-import dash_html_components as html
-import dash_core_components as dcc
 
 import torch
 import torch.distributions as d
@@ -13,165 +11,14 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-from flask_caching import Cache
-
+from retail import create_app
 from retail.retail import StoreEnv
 from retail.utility import CustomUtility
 
 
 name_df = pd.read_csv('Grocery_UPC_Database.csv')
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.config.suppress_callback_exceptions = True
-
-app.layout = html.Div(
-    [html.Div([
-        html.Div([html.H1(children='RetaiL Store Generator and Management'),
-                  html.Img(src=app.get_asset_url('grocery-beta.png'), style={'height': '70%', 'width': '70%'})],
-                 style={'columnCount': 2}),
-
-        dcc.Markdown('''
-    ### Manage your own store and see how it performs! 
-    The idea behind this web application is for you to customise your own store! But will you be able to avoid wasting?
-    Once you've opened your brand new store, it is time to define your scoring metric - will it help you in understanding the underlying dynamics? 
-    Will you be able to order the right amount of items, to reduce the ecological footprint of your store?
-    #### Store creation
-    The first step is to create your store. Enter below the characteristics to define your store. 
-    This will also generate an item assortment, which can be replicated by entering a seed.
-        '''),
-        html.Label('Store characteristics'),
-
-        html.Div([
-            html.Div([
-                html.Span('Customers: '),
-              dcc.Input(
-                  id="n_customers", type="number", value=500,
-                  min=0, max=2000, step=1,
-                )]),
-            html.Div([
-                html.Span('Items: '),
-                dcc.Input(
-                    id="n_items", type="number", value=30,
-                    min=5, max=100, step=1,
-                )]),
-            html.Div([
-                html.Span('Stock size: '),
-                dcc.Input(
-                    id="max_stock", type="number", value=600,
-                    min=0, max=2000, step=1,
-                )]),
-            html.Div([
-                html.Span('Horizon: '),
-                dcc.Input(
-                    id="horizon", type="number", value=91,
-                    min=40, max=1000, step=1,
-                )]),
-            html.Div([
-                html.Span('Bias: '),
-                dcc.Input(
-                    id="bias", type="number", value=0.0,
-                    min=-1, max=1, step=.01,
-                )]),
-            html.Div([
-                html.Span('Variance: '),
-                dcc.Input(
-                    id="variance", type="number", value=0.0,
-                    min=0, max=1, step=.01,
-                )]),
-            html.Div([
-                html.Span('ON Leadtime: '),
-                dcc.Input(
-                    id="leadtime_fast", type="number", value=0,
-                    min=0, max=10, step=1,
-                )]),
-            html.Div([
-                html.Span('ID Leadtime: '),
-                dcc.Input(
-                    id="leadtime_long", type="number", value=1,
-                    min=1, max=10, step=1,
-                )]),
-            html.Div([
-                html.Span('Buckets: '),
-                dcc.Input(
-                    id="daily_buckets", type="number", value=4,
-                    min=1, max=12, step=1,
-                )]),
-            html.Div([
-                html.Span('Seed: '),
-                dcc.Input(
-                    id="seed", type="number", placeholder="seed",
-                    min=1, max=100000, step=1,
-                )])], style={'columnCount': 5}),
-        dcc.Markdown('''
-    #### Success metric
-    Now, it is time to define your success metric. You can pick one and specify weights, or select "Custom" and enter your own metric - containing the letters "a" for availability, "s" for sales, and  "w" for waste.
-        '''),
-        html.Label('Utility function'),
-        dcc.Dropdown(id='utility_fun',
-                     options=[
-                         {'label': 'Cobb-Douglas', 'value': 'cobbdouglas'},
-                         {'label': 'Log-linear', 'value': 'loglinear'},
-                         {'label': 'Linear', 'value': 'linear'},
-                         {'label': 'Custom', 'value': 'custom'}
-                     ],
-                     value='cobbdouglas'
-                     ),
-        dcc.Input(
-            id="weight_waste", type="number", placeholder="Waste weight",
-            min=0, max=3, step=.01, value=0.5,
-        ),
-        dcc.Input(
-            id="weight_sales", type="number", placeholder="Sales weight",
-            min=0, max=3, step=.01, value=0.5,
-        ),
-        dcc.Input(
-            id="weight_availability", type="number", placeholder="Availability",
-            min=0, max=3, step=.01, value=0.5,
-        ),
-        html.Label('Custom utility'),
-        dcc.Input(type='text', id='utility', placeholder="Contains a,w and s"),
-
-        html.Label('Create store'),
-        html.Button('Create store', id='create'),
-        dcc.Markdown('''
-    #### Items
-    Below, you can see the items present in your store, with their price and cost. 
-    The color indicates how long you can keep them for.
-        '''),
-        dcc.Graph(id='output')], style={'width': '50%', 'float': 'left'}),
-
-        html.Div([dcc.Markdown('''
-    #### Freshness level
-    Freshness represents how long your items can be stored before the expiration date. The fresher the item, the shorter the time it can be stored! Overall, you can see this as the difficulty level. Try moving the slider! 
-        '''),
-                  html.Label('Freshness level'),
-                  dcc.Slider('freshness', min=1, max=100, value=1,
-                             marks={1: 'Standard',
-                                    10: 'Fresher items',
-                                    25: 'Very fresh',
-                                    50: 'Very very fresh',
-                                    75: 'Too fresh',
-                                    100: 'Bakery mode'}),
-
-                  dcc.Markdown('''
-    ### Order algorithm
-    The main goal is to adjust the number of each item you order for your store. To do so, you can build an expression around the following:
-    - n_customers, the number of customers expected
-    - forecast, the purchase probability of each of those customers
-    - stock, the current stock (or inventory position) of items.
-        '''),
-                  html.Label('Order input'),
-                  dcc.Input(value='forecast*n_customers - stock',
-                            type='text', id='order'),
-                  html.Label('Use your ordering policy'),
-                  html.Button('Order!', id='order_button'),
-                  dcc.Graph(id='output2')
-                  ], style={'marginLeft': '50%'})
-
-     ])
-
+app = create_app()
 
 @app.callback(
     Output('output', 'figure'),
@@ -237,11 +84,6 @@ def update_output_div(n_clicks, n_customers, n_items, max_stock, horizon,
                                 'Price': ":$,.2f",
                                 'Cost': ":$,.2f",
                                 'Shelf life at purchase': True})
-
-    # sc.update_layout(hovertemplate =
-   #                 '<b>%{Name}</b>'+
-   #                 '<br><i>Price</i>: €%{Price:.2f}'+
-   #                 '<br><i>Cost</i>: %{Cost:.2f}<br>')
     sc.update_yaxes(tickprefix="€")
     sc.update_xaxes(tickprefix="€")
     return(sc)
---
+++ retail/__init__.py
@@ -0,0 +1,161 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+
+
+def create_app():
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    app.config.suppress_callback_exceptions = True
+
+    # necessary to use flask.session
+    app.server.secret_key = b"SUPER_SECRET"
+
+    app.layout = html.Div(
+        [html.Div([
+            html.Div([html.H1(children='RetaiL Store Generator and Management'),
+                    html.Img(src=app.get_asset_url('grocery-beta.png'), style={'height': '70%', 'width': '70%'})],
+                    style={'columnCount': 2}),
+
+            dcc.Markdown('''
+        ### Manage your own store and see how it performs! 
+        The idea behind this web application is for you to customise your own store! But will you be able to avoid wasting?
+        Once you've opened your brand new store, it is time to define your scoring metric - will it help you in understanding the underlying dynamics? 
+        Will you be able to order the right amount of items, to reduce the ecological footprint of your store?
+        #### Store creation
+        The first step is to create your store. Enter below the characteristics to define your store. 
+        This will also generate an item assortment, which can be replicated by entering a seed.
+            '''),
+            html.Label('Store characteristics'),
+
+            html.Div([
+                html.Div([
+                    html.Span('Customers: '),
+                dcc.Input(
+                    id="n_customers", type="number", value=500,
+                    min=0, max=2000, step=1,
+                    )]),
+                html.Div([
+                    html.Span('Items: '),
+                    dcc.Input(
+                        id="n_items", type="number", value=30,
+                        min=5, max=100, step=1,
+                    )]),
+                html.Div([
+                    html.Span('Stock size: '),
+                    dcc.Input(
+                        id="max_stock", type="number", value=600,
+                        min=0, max=2000, step=1,
+                    )]),
+                html.Div([
+                    html.Span('Horizon: '),
+                    dcc.Input(
+                        id="horizon", type="number", value=91,
+                        min=40, max=1000, step=1,
+                    )]),
+                html.Div([
+                    html.Span('Bias: '),
+                    dcc.Input(
+                        id="bias", type="number", value=0.0,
+                        min=-1, max=1, step=.01,
+                    )]),
+                html.Div([
+                    html.Span('Variance: '),
+                    dcc.Input(
+                        id="variance", type="number", value=0.0,
+                        min=0, max=1, step=.01,
+                    )]),
+                html.Div([
+                    html.Span('ON Leadtime: '),
+                    dcc.Input(
+                        id="leadtime_fast", type="number", value=0,
+                        min=0, max=10, step=1,
+                    )]),
+                html.Div([
+                    html.Span('ID Leadtime: '),
+                    dcc.Input(
+                        id="leadtime_long", type="number", value=1,
+                        min=1, max=10, step=1,
+                    )]),
+                html.Div([
+                    html.Span('Buckets: '),
+                    dcc.Input(
+                        id="daily_buckets", type="number", value=4,
+                        min=1, max=12, step=1,
+                    )]),
+                html.Div([
+                    html.Span('Seed: '),
+                    dcc.Input(
+                        id="seed", type="number", placeholder="seed",
+                        min=1, max=100000, step=1,
+                    )])], style={'columnCount': 5}),
+            dcc.Markdown('''
+        #### Success metric
+        Now, it is time to define your success metric. You can pick one and specify weights, or select "Custom" and enter your own metric - containing the letters "a" for availability, "s" for sales, and  "w" for waste.
+            '''),
+            html.Label('Utility function'),
+            dcc.Dropdown(id='utility_fun',
+                        options=[
+                            {'label': 'Cobb-Douglas', 'value': 'cobbdouglas'},
+                            {'label': 'Log-linear', 'value': 'loglinear'},
+                            {'label': 'Linear', 'value': 'linear'},
+                            {'label': 'Custom', 'value': 'custom'}
+                        ],
+                        value='cobbdouglas'
+                        ),
+            dcc.Input(
+                id="weight_waste", type="number", placeholder="Waste weight",
+                min=0, max=3, step=.01, value=0.5,
+            ),
+            dcc.Input(
+                id="weight_sales", type="number", placeholder="Sales weight",
+                min=0, max=3, step=.01, value=0.5,
+            ),
+            dcc.Input(
+                id="weight_availability", type="number", placeholder="Availability",
+                min=0, max=3, step=.01, value=0.5,
+            ),
+            html.Label('Custom utility'),
+            dcc.Input(type='text', id='utility', placeholder="Contains a,w and s"),
+
+            html.Label('Create store'),
+            html.Button('Create store', id='create'),
+            dcc.Markdown('''
+        #### Items
+        Below, you can see the items present in your store, with their price and cost. 
+        The color indicates how long you can keep them for.
+            '''),
+            dcc.Graph(id='output')], style={'width': '50%', 'float': 'left'}),
+
+            html.Div([dcc.Markdown('''
+        #### Freshness level
+        Freshness represents how long your items can be stored before the expiration date. The fresher the item, the shorter the time it can be stored! Overall, you can see this as the difficulty level. Try moving the slider! 
+            '''),
+                    html.Label('Freshness level'),
+                    dcc.Slider('freshness', min=1, max=100, value=1,
+                                marks={1: 'Standard',
+                                        10: 'Fresher items',
+                                        25: 'Very fresh',
+                                        50: 'Very very fresh',
+                                        75: 'Too fresh',
+                                        100: 'Bakery mode'}),
+
+                    dcc.Markdown('''
+        ### Order algorithm
+        The main goal is to adjust the number of each item you order for your store. To do so, you can build an expression around the following:
+        - n_customers, the number of customers expected
+        - forecast, the purchase probability of each of those customers
+        - stock, the current stock (or inventory position) of items.
+            '''),
+                    html.Label('Order input'),
+                    dcc.Input(value='forecast*n_customers - stock',
+                                type='text', id='order'),
+                    html.Label('Use your ordering policy'),
+                    html.Button('Order!', id='order_button'),
+                    dcc.Graph(id='output2')
+                    ], style={'marginLeft': '50%'})
+
+        ])
+
+    return app
