a8de2947a1d6f114fb70b4c5877e7fa5bf52779b
Shubha Guha <shubha.g@gmail.com>
2020-11-30 15:37:44 +0100

    Converted store characteristics to table without visible lines

--- retail/layout.py
+++ retail/layout.py
@@ -24,59 +24,37 @@ def serve_layout_func(img):
                 The first step is to create your store. Enter below the characteristics to define your store.
                 This will also generate an item assortment, which can be replicated by entering a seed.
                 '''),
-                html.Label('Store characteristics'),
-                html.Div([
-                    html.Div([
-                        html.Span('Customers: '),
-                        dcc.Input(id="n_customers", type="number",
-                                  value=500, min=0, max=2000, step=1),
-                    ]),
-                    html.Div([
-                        html.Span('Items: '),
-                        dcc.Input(id="n_items", type="number",
-                                  value=30, min=5, max=100, step=1),
-                    ]),
-                    html.Div([
-                        html.Span('Stock size: '),
-                        dcc.Input(id="max_stock", type="number",
-                                  value=600, min=0, max=2000, step=1)
-                    ]),
-                    html.Div([
-                        html.Span('Horizon: '),
-                        dcc.Input(id="horizon", type="number",
-                                  value=91, min=40, max=1000, step=1),
-                    ]),
-                    html.Div([
-                        html.Span('Bias: '),
-                        dcc.Input(id="bias", type="number",
-                                  value=0.0, min=-1, max=1, step=.01),
-                    ]),
-                    html.Div([
-                        html.Span('Variance: '),
-                        dcc.Input(id="variance", type="number",
-                                  value=0.0, min=0, max=1, step=.01),
-                    ]),
-                    html.Div([
-                        html.Span('ON Leadtime: '),
-                        dcc.Input(id="leadtime_fast", type="number",
-                                  value=0, min=0, max=10, step=1),
-                    ]),
-                    html.Div([
-                        html.Span('ID Leadtime: '),
-                        dcc.Input(id="leadtime_long", type="number",
-                                  value=1, min=1, max=10, step=1),
-                    ]),
-                    html.Div([
-                        html.Span('Buckets: '),
-                        dcc.Input(id="daily_buckets", type="number",
-                                  value=4, min=1, max=12, step=1),
-                    ]),
-                    html.Div([
-                        html.Span('Seed: '),
-                        dcc.Input(id="seed", type="number",
-                                  placeholder="seed", min=1, max=100000, step=1),
-                    ])
-                ], style={'columnCount': 5}),
+                html.Table(title='Store characteristics', children=[
+                    html.Tr([
+                        html.Th('Store characteristics', colSpan='4'),
+                        html.Td('Seed'),
+                        html.Td(dcc.Input(id="seed", type="number", placeholder="seed", min=1, max=100000, step=1)),
+                    ], style={'border-style': 'hidden'}),
+                    html.Tr([
+                        html.Td('Customers'),
+                        html.Td(dcc.Input(id="n_customers", type="number", value=500, min=0, max=2000, step=1)),
+                        html.Td('Items'),
+                        html.Td(dcc.Input(id="n_items", type="number", value=30, min=5, max=100, step=1)),
+                        html.Td('Stock size'),
+                        html.Td(dcc.Input(id="max_stock", type="number", value=600, min=0, max=2000, step=1)),
+                    ], style={'border-style': 'hidden'}),
+                    html.Tr([
+                        html.Td('Horizon'),
+                        html.Td(dcc.Input(id="horizon", type="number", value=91, min=40, max=1000, step=1)),
+                        html.Td('Bias'),
+                        html.Td(dcc.Input(id="bias", type="number", value=0.0, min=-1, max=1, step=.01)),
+                        html.Td('Variance'),
+                        html.Td(dcc.Input(id="variance", type="number", value=0.0, min=0, max=1, step=.01)),
+                    ], style={'border-style': 'hidden'}),
+                    html.Tr([
+                        html.Td('ON Leadtime'),
+                        html.Td(dcc.Input(id="leadtime_fast", type="number", value=0, min=0, max=10, step=1)),
+                        html.Td('ID Leadtime'),
+                        html.Td(dcc.Input(id="leadtime_long", type="number", value=1, min=1, max=10, step=1)),
+                        html.Td('Buckets'),
+                        html.Td(dcc.Input(id="daily_buckets", type="number", value=4, min=1, max=12, step=1)),
+                    ], style={'border-style': 'hidden'}),
+                ]),
                 dcc.Markdown('''
                 #### Success metric
                 Now, it is time to define your success metric. You can pick one and specify weights, or select "Custom" and enter your own metric - containing the letters "a" for availability, "s" for sales, and  "w" for waste.
